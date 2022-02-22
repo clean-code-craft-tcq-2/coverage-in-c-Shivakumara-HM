52d81769f902115ec0cd49f0381951d6c069afed
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-02-22 22:57:13 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
+  if(value < lowerLimit) 
+  {
     return TOO_LOW;
   }
   if(value > upperLimit) {
